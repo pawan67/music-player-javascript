17d5bd15ac781c994b7a368e4e485e8f829bcbd6
unknown <pawantamada8@gmail.com>
2021-10-10 18:44:45 +0530

    bar

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function showHidePlaylist() {
 }
 // hide playlist
 function hidePlaylist() {
-  playlist.style.bottom = "-370px";
+  playlist.style.bottom = "-380px";
   console.log("you clicked me");
   arrDown.classList.remove("bgDown");
 }
